8829ff4751024198cdb1b896b592c35371a0d518
ACfuntion <109579595+ACfuntion@users.noreply.github.com>
2022-12-11 13:02:20 +0800

    Add files via upload

---
+++ qiuhe.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int Add(int x, int y)
+{
+	 int z = x + y;
+		return z;
+}
+int main()
+{
+	int num1 = 196;
+	int num2 = 427;
+	int sum = 0;
+	sum = Add(num1, num2);
+	printf("两个数的和为:%d\n", sum);
+	return 0;
+}
