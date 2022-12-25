6673ce28b202f864f1ee8f644724bfc691f76bff
ACfuntion <109579595+ACfuntion@users.noreply.github.com>
2022-12-25 11:08:22 +0800

    Add files via upload

---
+++ HANSHU.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	 int num = 0;
+	 while (num < 100)
+	 {
+		 num++;
+
+		 if (num % 2 ==1)
+			 printf("num = %d,ÆæÊý\n", num);
+		 else
+			 printf("num = %d,Å¼Êý\n", num);
+	 }
+	return 0;
+
+}
